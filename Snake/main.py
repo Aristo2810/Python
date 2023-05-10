cd10a60d7d874033a62f09d38a92e8c0cbd1bb26
Aristo2810 <aristo2110@gmail.com>
2023-05-10 18:49:37 +0200

    Code minimale de fonctionnement de pygame, constante de base

---
+++ Snake/main.py
@@ -0,0 +1,14 @@
+import pygame
+from constantes import *
+from pygame.locals import *
+
+
+pygame.init()
+
+fenetre = pygame.display.set_mode((LONGUEUR, LARGEUR))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = True

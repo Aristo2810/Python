7434579e17bc8d3d509dd56661e6b7affc44b5b2
Aristo2810 <aristo2110@gmail.com>
2023-05-10 20:14:32 +0200

    code minimal

--- Snake/main.py
+++ Snake/main.py
@@ -6,12 +6,15 @@ from pygame.locals import *
 pygame.init()
 
 fenetre = pygame.display.set_mode((LONGUEUR, LARGEUR))
-
+bg = pygame.image.load("assets/bg.jpg").convert()
+fenetre.blit(bg, (0, 0))
+pygame.display.update()
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = True
 
+    pygame.display.update()
 
 pygame.quit()

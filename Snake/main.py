d8d259551c83c058193e6f61c71e8e0f39261f52
Aristo2810 <aristo2110@gmail.com>
2023-05-11 20:06:37 +0200

    maj des fichiers

--- Snake/main.py
+++ Snake/main.py
@@ -4,17 +4,17 @@ from pygame.locals import *
 
 
 pygame.init()
-
 fenetre = pygame.display.set_mode((LONGUEUR, LARGEUR))
 bg = pygame.image.load("assets/bg.jpg").convert()
-fenetre.blit(bg, (0, 0))
-pygame.display.update()
+bg = pygame.transform.scale(bg, (LONGUEUR, LARGEUR))
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = True
 
-    pygame.display.update()
+    fenetre.fill((0, 0, 0))
+    fenetre.blit(bg, (0, 0))
+    pygame.display.flip()
 
 pygame.quit()

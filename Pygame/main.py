beb1d8986945793746adcb4e1ddcae987d7d4c01
Aristo2810 <aristo2110@gmail.com>
2023-05-13 13:19:17 +0200

    maj des fichiers

--- Pygame/main.py
+++ Pygame/main.py
@@ -3,10 +3,16 @@ import pygame
 
 pygame.init()
 
-fen = pygame.display.set_mode((800, 600))
-running = True
-while running:
-    for event in pygame.event.get():
-        if event == pygame.QUIT:
-            running = False
-pygame.quit()
+
+class Game:
+
+    def __int__(self):
+        self.fen = pygame.display.set_mode((800, 600))
+
+    def run_game(self) -> None:
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event == pygame.QUIT:
+                    running = False
+        pygame.quit()

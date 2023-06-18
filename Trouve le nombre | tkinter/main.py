b523e7e77e36801ee1a7ef264c4cb667e0443b16
aristo <aristo2110@gmail.com>
2023-06-18 21:53:00 +0200

    Ajout des bases du jeu, agencement du code en classe de front et de back

--- Trouve le nombre | tkinter/main.py
+++ Trouve le nombre | tkinter/main.py
@@ -2,5 +2,37 @@ import tkinter as tk
 import random
 
 
-fen = tk.Tk()
-fen.mainloop()
+class Front:
+
+    def __init__(self, fen):
+        self.rgbtohex = lambda r, g, b: f'#{r:02x}{g:02x}{b:02x}'
+        self.fen = fen
+        self.setup_display()
+
+    def setup_display(self):
+        self.fen.configure(bg=self.rgbtohex(36, 68, 92))
+        self.fen.geometry("1080x720")
+        self.entry = tk.Entry(self.fen, width=40)
+        self.entry.pack(pady=100)
+
+    def get_entry(self):
+        pass
+
+
+class Back:
+
+    def __init__(self, fen):
+        self.front = Front(fen)
+        self.random_numbers = random.randint(1, 100)
+        self.max_chance = 6
+        self.chance = 6
+
+
+def main():
+    fen = tk.Tk()
+    Back(fen)
+    fen.mainloop()
+
+
+if __name__ == '__main__':
+    main()

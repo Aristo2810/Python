ce4f756f0b394682ad9c7c4a764d16110f401eca
Aristo2810 <aristo2110@gmail.com>
2023-06-19 19:19:18 +0200

    Bug non patch : à corriger

--- Trouve le nombre | tkinter/main.py
+++ Trouve le nombre | tkinter/main.py
@@ -2,22 +2,24 @@ import tkinter as tk
 import random
 
 
-
 class Front:
 
     def __init__(self, fen):
         self.rgbtohex = lambda r, g, b: f'#{r:02x}{g:02x}{b:02x}'
         self.fen = fen
         self.setup_display()
+        self.back = Back(self.fen)
 
     def setup_display(self):
         self.fen.configure(bg=self.rgbtohex(36, 68, 92))
         self.fen.geometry("1080x720")
         self.entry = tk.Entry(self.fen, width=40)
+        self.var = tk.StringVar()
         self.entry.pack(pady=100)
-
-    def get_entry(self):
-        pass
+        self.label = tk.Label(self.fen, bg=self.rgbtohex(36, 68, 92), textvariable=self.var)
+        self.label.pack()
+        # self.btn = tk.Button(self.fen, command=self.back.get_entry, width=15, text="Valider")
+        # self.btn.pack()
 
 
 class Back:
@@ -27,6 +29,18 @@ class Back:
         self.random_numbers = random.randint(1, 100)
         self.max_chance = 6
         self.chance = 6
+
+    def plus_ou_moins(self, nb_a_tester: int) -> int:
+        if nb_a_tester == self.random_numbers:
+            return 1
+        else:
+            if nb_a_tester > self.random_numbers:
+                return 2
+            else:
+                return 0
+
+    def get_entry(self):
+        print(1)
 
 
 def main():

00b868fc81a26118b60655416a10868eb824b208
Aristo2810 <aristo2110@gmail.com>
2023-06-21 18:07:58 +0200

    Projet en pause

--- Projet perso/Trouve le nombre | tkinter/main.py
+++ Projet perso/Trouve le nombre | tkinter/main.py
@@ -1,51 +1,42 @@
-import tkinter as tk
 import random
+import tkinter as tk
 
 
-class Front:
+class Game:
 
     def __init__(self, fen):
-        self.rgbtohex = lambda r, g, b: f'#{r:02x}{g:02x}{b:02x}'
         self.fen = fen
-        self.setup_display()
-        self.back = Back(self.fen)
-
-    def setup_display(self) -> None:
+        self.vie = 6
+        self.proposition = 0
+        self.rgbtohex = lambda r, g, b: f'#{r:02x}{g:02x}{b:02x}'
         self.fen.configure(bg=self.rgbtohex(36, 68, 92))
         self.fen.geometry("1080x720")
-        self.entry = tk.Entry(self.fen, width=40)
         self.var = tk.StringVar()
-        self.entry.pack(pady=100)
+        self.entry = tk.Entry(self.fen, width=40)
+        self.entry.pack(pady=60)
         self.label = tk.Label(self.fen, bg=self.rgbtohex(36, 68, 92), textvariable=self.var)
         self.label.pack()
-        # self.btn = tk.Button(self.fen, command=self.back.get_entry, width=15, text="Valider")
-        # self.btn.pack()
-
-
-class Back:
-
-    def __init__(self, fen):
-        self.front = Front(fen)
-        self.random_numbers = random.randint(1, 100)
-        self.max_chance = 6
-        self.chance = 6
-
-    def plus_ou_moins(self, nb_a_tester: int) -> int:
-        if nb_a_tester == self.random_numbers:
-            return 1
-        else:
-            if nb_a_tester > self.random_numbers:
-                return 2
-            else:
-                return 0
+        self.btn = tk.Button(self.fen, width=15, text="Valider", command=self.get_entry)
+        self.btn.pack()
 
     def get_entry(self):
-        print(1)
+        self.proposition = int(self.entry.get())
+
+    @staticmethod
+    def generate_random_numbers():
+        return random.randint(1, 100)
+
+    def ingame(self):
+        while self.vie > 0:
+            random_numbers = self.generate_random_numbers()
+            if random_numbers == self.proposition:
+
+
 
 
 def main():
     fen = tk.Tk()
-    Back(fen)
+    Game(fen)
     fen.mainloop()
 
 
